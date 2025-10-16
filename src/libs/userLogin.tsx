3b7400445ff74bf02710440912fdd1899a36fc94
lattachot1 <freefox1121@email.com>
2025-10-16 20:59:55 +0700

    fix name LogIn

--- src/libs/userLogin.tsx
+++ src/libs/userLogin.tsx
@@ -1,6 +1,6 @@
 
 
-export default async function userLogin(userEmail: string, userPassword: string) {
+export default async function userLogIn(userEmail: string, userPassword: string) {
 
    const response =  await fetch('https://a08-venue-explorer-backend-2.vercel.app/api/v1/auth/login', {
         method: 'POST',
